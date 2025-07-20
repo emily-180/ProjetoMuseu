d0ebc90b792ddb853a8c6a3840bca419d49fa3d3
emily-180 <emily.moreira.8@gmail.com>
2025-07-20 14:23:49 -0300

    commit 7 emily

--- views/visitas.php
+++ views/visitas.php
@@ -19,28 +19,28 @@ $membros = $membroModel->listar();
     <section class="acervo">
       <div class="grid-acervo">
         <div class="item-acervo">
-          <img src="/ProjetoMuseu/static/imagens/temporaria.png" alt="Ambiente da Caatinga">
+          <img src="/ProjetoMuseu/static/imagens/caatinga.jpeg" alt="Ambiente da Caatinga">
           <div class="info">
             <p class="tag">ambiente</p>
             <h3>Caatinga</h3>
           </div>
         </div>
         <div class="item-acervo">
-          <img src="/ProjetoMuseu/static/imagens/temporaria.png" alt="Ambiente do Cerrado">
+          <img src="/ProjetoMuseu/static/imagens/cerrado.jpeg" alt="Ambiente do Cerrado">
           <div class="info">
             <p class="tag">ambiente</p>
             <h3>Cerrado</h3>
           </div>
         </div>
         <div class="item-acervo">
-          <img src="/ProjetoMuseu/static/imagens/temporaria.png" alt="Ambiente da Amazônia">
+          <img src="/ProjetoMuseu/static/imagens/mata.jpeg" alt="Ambiente da Amazônia">
           <div class="info">
             <p class="tag">ambiente</p>
             <h3>Mata Atântica</h3>
           </div>
         </div>
         <div class="item-acervo">
-          <img src="/ProjetoMuseu/static/imagens/temporaria.png" alt="Ambiente Interno do Museu">
+          <img src="/ProjetoMuseu/static/imagens/marinho.jpeg" alt="Ambiente Interno do Museu">
           <div class="info">
             <p class="tag">ambiente</p>
             <h3>Ecossitema Marinho</h3>
